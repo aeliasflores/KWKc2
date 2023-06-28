4236f6abdb2e8f9ef9afb1614aaa95e83598eb6a
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-06-28 14:13:34 -0400

    Initial Commit

---
+++ KWKc2/KWKc2App.swift
@@ -0,0 +1,17 @@
+//
+//  KWKc2App.swift
+//  KWKc2
+//
+//  Created by Michelle Elias Flores on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct KWKc2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
